a26d0b80f1460f6377d0a1d05a16e7c9ebba7ae1
Michael Hearne <mhearne@usgs.gov>
2014-06-26 15:29:35 -0600

    Now rotating the data through back-azimuth specified in the header

--- smtools/iran.py
+++ smtools/iran.py
@@ -19,6 +19,7 @@ from obspy.core.trace import Trace
 from obspy.core.trace import Stats
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.core.util.geodetics import gps2DistAzimuth
+from obspy.signal import rotate
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -121,6 +122,24 @@ def readiran(iranfile):
         hdrlines = readheaderlines(f)
 
     f.close()
+    
+    #data from Iran may be rotated so that one channel is aligned in the direction between the earthquake
+    #epicenter and the station.  We want to rotate the data back so that we have what are presumably the 
+    #original NS and EW channels.  We presume that the "L" channel will rotate back to become NS, and
+    #"T" will become EW.
+    #First, find the channel called L*
+    channels = [h['channel'][0:1] for h in headerlist]
+    lidx = channels.index('L')
+    tidx = channels.index('T')
+    ldata = tracelist[lidx].data
+    tdata = tracelist[tidx].data
+    backaz = headerlist[0]['rotation']['L']
+    ndata,edata = rotate.rotate_RT_NE(ldata,tdata,backaz)
+    tracelist[lidx].data = ndata.copy()
+    tracelist[lidx].stats['channel'] = 'H1' #most probably NS, but we're being cautious
+    tracelist[tidx].data = edata.copy()
+    tracelist[tidx].stats['channel'] = 'H2' #most probably EW, but we're being cautious
+    
     return (tracelist,headerlist)
 
 if __name__ == '__main__':
